6faf07686cb91ea3d8eaa35ab3274c047e0e9fdb
Michal Tomczyk <michaltomczyk28@gmail.com>
2023-06-18 15:46:44 +0200

    feat: add database migration for account table

--- src/main/java/server/database/ResetDatabaseMigration.java
+++ src/main/java/server/database/ResetDatabaseMigration.java
@@ -10,8 +10,14 @@ public class ResetDatabaseMigration {
         Statement statement = db.getConnection().createStatement();
 
         statement.executeUpdate("drop table if exists user");
-        statement.executeUpdate("create table user (id integer, firstname string, lastname string, pesel string, username string, password string)");
+        statement.executeUpdate("create table user (id integer, firstname string, lastname string, pesel string, username string, password string, primary key (id))");
+
+        statement.executeUpdate("drop table if exists account");
+        statement.executeUpdate("create table account (account_number string, user_id integer, balance real, primary key (account_number), foreign key (user_id) references user(id))");
+
+
         statement.executeUpdate("insert into user values(1, 'Jan', 'Kowalski', '01220314894', 'jankowalski', 'qwerty123')");
+        statement.executeUpdate("insert into account values('PL37240463090473155465742491', 1, 120.3)");
 
         ResultSet rs = statement.executeQuery("select name from sqlite_schema where type = 'table' and name not like 'sqlite_%'");
         System.out.println("The database has been reset.");
